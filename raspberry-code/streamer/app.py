b286da336151691618e2909399526f9b6dfa79c4
Bakel Bakel <begededum4bakel@gmail.com>
2025-05-10 01:58:19 +0200

    exclude trains from detection

--- raspberry-code/streamer/app.py
+++ raspberry-code/streamer/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, Response, render_template
 from ultralytics import YOLO
 import cv2
+import numpy as np
 
 app = Flask(__name__)
 model = YOLO('yolov8s.pt')  #
@@ -10,23 +11,35 @@ camera = cv2.VideoCapture(0)
 camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
+
 def generate_frames():
     while True:
         success, frame = camera.read()
         if not success:
             break
-        
-        # Inference
+
+        # Run inference
         results = model.predict(source=frame, imgsz=640, conf=0.3, verbose=False)
+
+        # Get bounding boxes
+        boxes = results[0].boxes
+        if boxes is not None:
+            # Filter out class ID 7 = 'train'
+            exclude_ids = [7]  # Add more like [6, 7, 8] to exclude bus, train, truck
+            filtered_boxes = boxes[~np.isin(boxes.cls.cpu().numpy(), exclude_ids)]
+            results[0].boxes = filtered_boxes  # Replace with filtered boxes
+
+        # Annotate the frame
         annotated_frame = results[0].plot()
 
-        # Encode as JPEG
+        # Encode the frame to JPEG
         ret, buffer = cv2.imencode('.jpg', annotated_frame)
         frame = buffer.tobytes()
 
-        # Yield frame in MJPEG format
+        # Yield frame for streaming
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 
 @app.route('/')
 def index():
